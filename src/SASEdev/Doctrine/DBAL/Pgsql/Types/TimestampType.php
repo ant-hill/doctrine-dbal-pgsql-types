9e54c75bfc611489ebdb3fbe070c2c76b890e790
Config Master <unit.1985@gmail.com>
2014-09-04 19:48:08 +0400

    add timestamp type

---
+++ src/SASEdev/Doctrine/DBAL/Pgsql/Types/TimestampType.php
@@ -0,0 +1,75 @@
+<?php
+namespace SASEdev\Doctrine\DBAL\Pgsql\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class Timestamp extends Type
+{
+
+    const TIMESTAMP = 'Timestamp';
+
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     *
+     * @param array $fieldDeclaration The field declaration.
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform The currently used database platform.
+     *
+     * @return string
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return self::TIMESTAMP;
+    }
+
+    /**
+     * Converts a value from its PHP representation to its database representation
+     * of this type.
+     *
+     * @param mixed $value The value to convert.
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform The currently used database platform.
+     *
+     * @return mixed The database representation of the value.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (!$value) {
+            return null;
+        }
+        /* @var $value \DateTime */
+        return $value->getTimestamp();
+    }
+
+    /**
+     * Converts a value from its database representation to its PHP representation
+     * of this type.
+     *
+     * @param mixed $value The value to convert.
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform The currently used database platform.
+     *
+     * @return mixed The PHP representation of the value.
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (!$value) {
+            return null;
+        }
+
+        $dateTime = new \DateTime();
+        $dateTime->setTimestamp($value);
+
+        return $dateTime;
+    }
+
+    /**
+     * Gets the name of this type.
+     *
+     * @return string
+     *
+     * @todo Needed?
+     */
+    public function getName()
+    {
+        return self::TIMESTAMP;
+    }
+}
